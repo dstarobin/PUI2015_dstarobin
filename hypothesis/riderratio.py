90b14c5f325d61e04df5098739e79db9a8f5139a
dstarobin <ds2455@nyu.edu>
2015-10-04 12:00:44 -0400

    Create riderratio.py

---
+++ hypothesis/riderratio.py
@@ -0,0 +1,29 @@
+#  cd dropbox/cusp/pui/pui2015_ds2455_hw3
+import pandas as pd
+from sys import argv
+
+csv = argv[1]
+
+if __name__=='__main__':
+	print "Working--this might take a while (approx 2 seconds per MB in CSV). Please be patient..."
+	bikes = pd.read_csv(csv, parse_dates=['starttime'])
+	#values 5,6 are Saturday and Sunday
+	days = bikes['starttime'].dt.dayofweek
+	bikes['dayvals'] = days
+
+	weekends = bikes[(bikes.dayvals == 5) | (bikes.dayvals == 6)] 
+	weekdays = bikes[(bikes.dayvals != 5) & (bikes.dayvals != 6)]
+
+	custWknds = weekends[weekends.usertype == 'Customer']
+	subWknds = weekends[weekends.usertype == 'Subscriber']
+
+	custWkday = weekdays[weekdays.usertype == 'Customer']
+	subWkday = weekdays[weekdays.usertype == 'Subscriber']
+
+	print 'Weekend customer rides = %.2f' % (len(custWknds))
+	print 'Weekend subscriber rides = %.2f' % (len(subWknds))
+	print "Weekend subscriber: weekend customer ratio = %.2f" % (float(len(subWknds))/float(len(custWknds)))
+
+	print 'Weekday customer rides = %.2f' % (len(custWkday))
+	print 'Weekday subscriber rides = %.2f' % (len(subWkday))
+	print "Weekday subscriber: weekday customer ratio = %.2f" % (float(len(subWkday))/float(len(custWkday)))
